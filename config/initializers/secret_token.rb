80c20cd92309d2b19787a01dc3ae6ff0b7d7fa82
ankit gupta <ankit.gupta8898@gmail.com>
2011-06-27 14:39:55 +0530

    moved

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForMove::Application.config.secret_token = '62cfecb4de3049ebc8f3126a8ecd5959367464b8bb55da6bdacec50368b899394932490b878c4610da9b3ecd7ba315668cc569e420a5a09a7df31b692a59131b'
